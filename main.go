9426101dfe2288f26b94fbd85ee04e101615e433
Moch Sultan Majid <sultan.majid213@gmail.com>
2023-11-15 22:12:13 +0700

    chore: set logger

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/ExeCiety/be-presensi-comindo/cmd"
 	"github.com/ExeCiety/be-presensi-comindo/db"
 	pkgRouters "github.com/ExeCiety/be-presensi-comindo/pkg/routers"
 	"github.com/ExeCiety/be-presensi-comindo/utils"
 	utilsEnums "github.com/ExeCiety/be-presensi-comindo/utils/enums"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -30,6 +32,31 @@ func main() {
 
 	// Create Fiber app
 	app := fiber.New()
+
+	// Configure Logger
+	if _, err := os.Stat("logs"); os.IsNotExist(err) {
+		if errMkDirLog := os.Mkdir("logs", 0755); errMkDirLog != nil {
+			panic(errMkDirLog)
+		}
+	}
+
+	logFilePath := filepath.Join("logs", "logs.log")
+	logFile, setLogError := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if setLogError != nil {
+		panic(setLogError)
+	}
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal("Failed to close log file")
+		}
+	}(logFile)
+
+	app.Use(logger.New())
+	app.Use(logger.New(logger.Config{
+		Output: logFile,
+	}))
 
 	// Set Routers
 	pkgRouters.SetRouter(app)
